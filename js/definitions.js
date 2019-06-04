47b8205940ff5c869c51316da94ad15875ebe183
peirung <49450891+peirung@users.noreply.github.com>
2019-06-03 18:38:03 -0700

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,261 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "His-tagged Protein Purification",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/peirung/Protein_purification_v0603/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+            {
+              name: 'Column Packing',
+              path: 'operation_types/Column_Packing' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Concentrate Protein',
+              path: 'operation_types/Concentrate_Protein' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Harvest Cell',
+              path: 'operation_types/Harvest_Cell' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'His-tagged Purification',
+              path: 'operation_types/His-tagged_Purification' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'IPTG Induction',
+              path: 'operation_types/IPTG_Induction' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Lyse Cell',
+              path: 'operation_types/Lyse_Cell' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Lysis Buffer Preparation',
+              path: 'operation_types/Lysis_Buffer_Preparation' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Make IPTG Aliquots',
+              path: 'operation_types/Make_IPTG_Aliquots' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Make Imidazole Aliquots',
+              path: 'operation_types/Make_Imidazole_Aliquots' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Make Overexpression',
+              path: 'operation_types/Make_Overexpression' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Make a Starter',
+              path: 'operation_types/Make_a_Starter' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Purification Buffer Preparation',
+              path: 'operation_types/Purification_Buffer_Preparation' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'SDS-PAGE Analysis',
+              path: 'operation_types/SDS-PAGE_Analysis' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'SDS-PAGE Gel Casting',
+              path: 'operation_types/SDS-PAGE_Gel_Casting' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'SDS-PAGE Sample Preparation',
+              path: 'operation_types/SDS-PAGE_Sample_Preparation' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Scan Gel',
+              path: 'operation_types/Scan_Gel' + '.md',
+              type: "local-md"
+            },
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+            {
+              name: 'Assay Buffer',
+              path: 'sample_types/Assay_Buffer'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Plasmid',
+              path: 'sample_types/Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Primer',
+              path: 'sample_types/Primer'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Protein',
+              path: 'sample_types/Protein'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Resin',
+              path: 'sample_types/Resin'  + '.md',
+              type: "local-md"
+            },
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+            {
+              name: '12% SDS Gel in Coomassie Staining',
+              path: 'object_types/12_SDS_Gel_in_Coomassie_Staining'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: '12% SDS Gel in Tank',
+              path: 'object_types/12_SDS_Gel_in_Tank'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: '500 mL Liquid',
+              path: 'object_types/500_mL_Liquid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Cell Lysate of Protein Expression',
+              path: 'object_types/Cell_Lysate_of_Protein_Expression'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Cell Pellet of protein',
+              path: 'object_types/Cell_Pellet_of_protein'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Checked E coli Plate of Plasmid',
+              path: 'object_types/Checked_E_coli_Plate_of_Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Column',
+              path: 'object_types/Column'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Concentrated Protein',
+              path: 'object_types/Concentrated_Protein'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Laemmli Sample Buffer',
+              path: 'object_types/Laemmli_Sample_Buffer'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Overexpression of Plasmid',
+              path: 'object_types/Overexpression_of_Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Plasmid Glycerol Stock',
+              path: 'object_types/Plasmid_Glycerol_Stock'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'Protein Elution',
+              path: 'object_types/Protein_Elution'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'TB Overnight (400 mL) of Plasmid',
+              path: 'object_types/TB_Overnight_400_mL_of_Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'TB Overnight of Plasmid',
+              path: 'object_types/TB_Overnight_of_Plasmid'  + '.md',
+              type: "local-md"
+            },
+          
+        ]
+      }
+    
+
+  ]
+
+};
